d376875c3acfd372ee9969197ca8440bea4f4b61
Muccachino <lucas_staszewski@yahoo.de>
2023-12-20 10:43:41 +0100

    add several methods for singly linked list

--- singly_linked_list/singly_linked_list.js
+++ singly_linked_list/singly_linked_list.js
@@ -75,6 +75,71 @@ class SinglyLinkedList {
         this.length++;
         return this;
     }
+    get(index) {
+        if (index < 0 || index >= this.length)
+            return null;
+        let currentItem = this.head;
+        for (let i = 0; i < index; i++) {
+            currentItem = currentItem.next;
+        }
+        return currentItem;
+    }
+    set(value, index) {
+        let searchNode = this.get(index);
+        if (!searchNode)
+            return false;
+        searchNode.value = value;
+        return true;
+    }
+    insert(value, index) {
+        if (index < 0 || index > this.length)
+            return -1;
+        if (index === 0)
+            return this.unshift(value);
+        if (index === this.length)
+            return this.append(value);
+        const newNode = new ListNode(value);
+        let preNode = this.get(index - 1);
+        newNode.next = preNode === null || preNode === void 0 ? void 0 : preNode.next;
+        preNode.next = newNode;
+        this.length++;
+        return this;
+    }
+    remove(index) {
+        var _a;
+        if (index < 0 || index >= this.length)
+            return -1;
+        if (index === 0)
+            return this.shift();
+        if (index === this.length - 1)
+            return this.pop();
+        const removeNode = this.get(index);
+        let preNode = this.get(index - 1);
+        preNode.next = (_a = preNode.next) === null || _a === void 0 ? void 0 : _a.next;
+        this.length--;
+        return removeNode;
+    }
+    reverse() {
+        if (!this.head)
+            return null;
+        if (this.length === 1)
+            return this;
+        let oldTail = this.tail;
+        let oldHead = this.head;
+        this.tail = oldHead;
+        this.head = oldTail;
+        let prev;
+        let next;
+        let node = this.tail;
+        for (let i = 0; i < this.length; i++) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        this.tail.next = null;
+        return this;
+    }
 }
 const myList = new SinglyLinkedList();
 myList.append(5);
@@ -87,4 +152,14 @@ console.log("After POP", myList);
 console.log("SHIFT Item", myList.shift());
 console.log("After SHIFT", myList);
 console.log(myList.unshift(200));
+myList.append(10);
+myList.append(20);
+console.log(myList);
+console.log("GET Index", myList.get(1));
+console.log("SET Index", myList.set(3000, 1));
+console.log("After SET", myList);
+console.log("After INSERT", myList.insert(500, 1));
+console.log("REMOVED Item", myList.remove(1));
+console.log("After REMOVE", myList);
+console.log("After REVERSE", myList.reverse());
 //# sourceMappingURL=singly_linked_list.js.map
--- singly_linked_list/singly_linked_list.ts
+++ singly_linked_list/singly_linked_list.ts
@@ -81,6 +81,81 @@ class SinglyLinkedList {
     this.length++;
     return this
   }
+
+  get(index: number) {
+    if (index < 0 || index >= this.length) return null;
+    let currentItem: ListNode | null = this.head;
+
+    for (let i = 0; i < index; i++) {
+      currentItem = currentItem!.next;
+    }
+
+    return currentItem;
+
+  }
+
+  set(value: number, index: number) {
+    let searchNode = this.get(index)
+    if (!searchNode) return false;
+
+    searchNode.value = value;
+
+    return true;
+  }
+
+  insert(value: number, index: number) {
+    if (index < 0 || index > this.length) return -1;
+    if (index === 0) return this.unshift(value);
+    if (index === this.length) return this.append(value);
+
+    const newNode = new ListNode(value);
+    let preNode = this.get(index-1)
+
+    newNode.next = preNode?.next as ListNode;
+    preNode!.next = newNode as ListNode;
+
+    this.length++;
+    return this;
+
+  }
+
+  remove(index: number) {
+    if(index < 0 || index >= this.length) return -1;
+    if(index === 0) return this.shift();
+    if(index === this.length - 1) return this.pop();
+
+    const removeNode = this.get(index) as ListNode;
+    let preNode = this.get(index - 1);
+    preNode!.next = preNode!.next?.next as ListNode | null
+
+    this.length--;
+
+    return removeNode;
+  } 
+
+  reverse() {
+    if(!this.head) return null;
+    if(this.length === 1) return this;
+
+    let oldTail = this.tail;
+    let oldHead = this.head;
+    this.tail = oldHead;
+    this.head = oldTail;
+
+    let prev: ListNode | null;
+    let next: ListNode | null;
+    let node: ListNode | null = this.tail;
+
+    for (let i = 0; i < this.length; i++) {
+      next = node!.next;
+      node!.next = prev!
+      prev = node
+      node = next;
+    }
+    this.tail.next = null;
+
+    return this;
+  }
 }
 
 const myList = new SinglyLinkedList();
@@ -98,3 +173,18 @@ console.log("SHIFT Item",myList.shift());
 console.log("After SHIFT",myList);
 
 console.log(myList.unshift(200));
+
+myList.append(10);
+myList.append(20);
+
+console.log(myList);
+console.log("GET Index", myList.get(1));
+
+console.log("SET Index", myList.set(3000, 1));
+console.log("After SET", myList);
+
+console.log("After INSERT", myList.insert(500, 1));
+console.log("REMOVED Item", myList.remove(1));
+console.log("After REMOVE", myList);
+
+console.log("After REVERSE", myList.reverse());
